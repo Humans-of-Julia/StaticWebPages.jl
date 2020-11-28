4fce7a89c264cb1003dde2cd1ec0f0d0420369c8
azzaare <jf@baffier.fr>
2020-11-29 08:29:54 +0900

    Added CI. Tagged new version v0.2.5

---
+++ test/run.jl
@@ -0,0 +1,22 @@
+# import the website generator functions
+import StaticWebPages
+import StaticWebPages: local_info
+
+## private information (local folders, connection infos, etc.)
+# content and site paths are always required
+local_info["content"] = "../example"
+local_info["site"] = "build"
+
+# necessary only if using the upload_site function
+local_info["protocol"] = "ftp"
+local_info["user"] = "user"
+local_info["password"] = "password"
+local_info["server"] = "server_address"
+
+# `rm_dir = true` will clean up the site folder before generating it again. Default to false.
+# `opt_in = true` will a link to this generator website in the side menu. Default to false.
+StaticWebPages.export_site(d=local_info, rm_dir = true, opt_in = true)
+
+## upload website (comment/delete if not needed)
+# unfortunately does not work yet on windows system, please sync manually for the moment
+# StaticWebPages.upload_site(local_info)
---
+++ test/runtests.jl
@@ -0,0 +1,36 @@
+using StaticWebPages
+using Test
+
+@testset "StaticWebPages.jl" begin
+mkdir("build")
+include("run.jl")
+
+# Personal file existence
+@test isfile("build/index.html")
+@test isfile("build/publications.html")
+@test isfile("build/research.html")
+@test isfile("build/software.html")
+
+@test isfile("build/img/bmilp.png")
+@test isfile("build/img/cs.png")
+@test isfile("build/img/knowledge.png")
+@test isfile("build/img/pic.jpg")
+
+@test isfile("build/files/bae2018gapplanar.pdf")
+@test isfile("build/files/baffier2017hanabi.pdf")
+@test isfile("build/files/baffier2018experimental.pdf")
+@test isfile("build/files/cv.pdf")
+@test isfile("build/files/parmentier2019introducing.pdf")
+@test isfile("build/files/richoux2016ghost.pdf")
+
+# Template file existence
+@test isfile("build/css/app.css")
+@test isfile("build/css/foundation.min.css")
+
+@test isfile("build/js/app.js")
+@test isfile("build/js/vendor/foundation.min.js")
+@test isfile("build/js/vendor/jquery.js")
+@test isfile("build/js/vendor/what-input.js")
+
+rm("build", recursive=true)
+end
