0f91f372d4da715e1a4fda52d128f3d197998f0e
Documenter.jl <documenter@juliadocs.github.io>
2021-04-29 14:07:27 +0000

    build based on fad0708

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = StaticWebPages","category":"page"},{"location":"#StaticWebPages","page":"Home","title":"StaticWebPages","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [StaticWebPages]","category":"page"},{"location":"#StaticWebPages.GitBuilder-Tuple{GitHub.Repo, String}","page":"Home","title":"StaticWebPages.GitBuilder","text":"GitBuilder(r, contributors)\n\nGenerate the Git struct according to the repository r. If r does not have all its fields specified (i.e. Nothing), then a default value is used instead.\n\nReturn g::Git\n\n\n\n\n\n","category":"method"}]
+}
